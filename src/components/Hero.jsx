2c072ddf27f75271942ec43065b356f963fb125e
Mehmet Fatih Nazıroğlu <m.fatihnaziroglu@gmail.com>
2023-08-30 16:40:57 +0300

    30.08.23 16:40

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,7 +26,7 @@ const Hero = () => {
 
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
-          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+          <div className="w-[35px] h-[60px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.dev
               animate={{
                 y:[0, 25, 0]
